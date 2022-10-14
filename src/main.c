9af43c031b3c77c302e7f0026a4393aa7e7f7e2a
Henrik Nordström <henrik.nordstrom@addiva.se>
2022-10-14 13:43:46 +0200

    Configure project as a CAF project

--- src/main.c
+++ src/main.c
@@ -1,111 +1,28 @@
 /*
- * Class A LoRaWAN sample application
- *
- * Copyright (c) 2020 Manivannan Sadhasivam <mani@kernel.org>
+ * Copyright (c) 2022 Addiva Elektronik AB
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/device.h>
-#include <zephyr/lorawan/lorawan.h>
+#define MODULE main
+
 #include <zephyr/zephyr.h>
-
-/* Customize based on network configuration */
-#define LORAWAN_DEV_EUI			{ 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x05, 0x4D, 0x7E }
-#define LORAWAN_JOIN_EUI		{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
-#define LORAWAN_APP_KEY			{ 0xF0, 0x48, 0x8E, 0x51, 0x3A, 0xB3, 0x83, 0xBB, 0x35, 0x71, 0x1C, 0x4E, 0xE6, 0x0E, 0x89, 0x25 }
-
-#define DELAY K_MSEC(10000)
-
-#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <app_event_manager.h>
+#include <caf/events/module_state_event.h>
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(lorawan_class_a);
 
-char data[] = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'};
-
-static void dl_callback(uint8_t port, bool data_pending,
-			int16_t rssi, int8_t snr,
-			uint8_t len, const uint8_t *data)
-{
-	LOG_INF("Port %d, Pending %d, RSSI %ddB, SNR %ddBm", port, data_pending, rssi, snr);
-	if (data) {
-		LOG_HEXDUMP_INF(data, len, "Payload: ");
-	}
-}
-
-static void lorwan_datarate_changed(enum lorawan_datarate dr)
-{
-	uint8_t unused, max_size;
-
-	lorawan_get_payload_sizes(&unused, &max_size);
-	LOG_INF("New Datarate: DR_%d, Max Payload %d", dr, max_size);
-}
+LOG_MODULE_REGISTER(MODULE);
 
 void main(void)
 {
-	const struct device *lora_dev;
-	struct lorawan_join_config join_cfg;
-	uint8_t dev_eui[] = LORAWAN_DEV_EUI;
-	uint8_t join_eui[] = LORAWAN_JOIN_EUI;
-	uint8_t app_key[] = LORAWAN_APP_KEY;
-	int ret;
+	LOG_INF("Starting");
 
-	struct lorawan_downlink_cb downlink_cb = {
-		.port = LW_RECV_PORT_ANY,
-		.cb = dl_callback
-	};
-
-	lora_dev = DEVICE_DT_GET(DT_ALIAS(lora0));
-	if (!device_is_ready(lora_dev)) {
-		LOG_ERR("%s: device not ready.", lora_dev->name);
-		return;
+	// Start CAF and signal that main have completed the system setup
+	if (app_event_manager_init()) {
+		LOG_ERR("Application Event Manager not initialized");
+	} else {
+		module_set_state(MODULE_STATE_READY);
 	}
 
-	ret = lorawan_start();
-	if (ret < 0) {
-		LOG_ERR("lorawan_start failed: %d", ret);
-		return;
-	}
-
-	lorawan_register_downlink_callback(&downlink_cb);
-	lorawan_register_dr_changed_callback(lorwan_datarate_changed);
-
-	join_cfg.mode = LORAWAN_ACT_OTAA;
-	join_cfg.dev_eui = dev_eui;
-	join_cfg.otaa.join_eui = join_eui;
-	join_cfg.otaa.app_key = app_key;
-	join_cfg.otaa.nwk_key = app_key;
-
-	LOG_INF("Joining network over OTAA");
-	ret = lorawan_join(&join_cfg);
-	if (ret < 0) {
-		LOG_ERR("lorawan_join_network failed: %d", ret);
-		return;
-	}
-
-	LOG_INF("Sending data...");
-	while (1) {
-		ret = lorawan_send(2, data, sizeof(data),
-				   LORAWAN_MSG_CONFIRMED);
-
-		/*
-		 * Note: The stack may return -EAGAIN if the provided data
-		 * length exceeds the maximum possible one for the region and
-		 * datarate. But since we are just sending the same data here,
-		 * we'll just continue.
-		 */
-		if (ret == -EAGAIN) {
-			LOG_ERR("lorawan_send failed: %d. Continuing...", ret);
-			k_sleep(DELAY);
-			continue;
-		}
-
-		if (ret < 0) {
-			LOG_ERR("lorawan_send failed: %d", ret);
-			return;
-		}
-
-		LOG_INF("Data sent!");
-		k_sleep(DELAY);
-	}
+	LOG_INF("Done");
 }
