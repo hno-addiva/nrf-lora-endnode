e8202b40e03251740fb8ea1e097169544c58938f
Henrik Nordström <henrik.nordstrom@addiva.se>
2022-10-17 14:18:22 +0200

    Load settings on application startup

--- src/main.c
+++ src/main.c
@@ -10,12 +10,19 @@
 #include <app_event_manager.h>
 #include <caf/events/module_state_event.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/settings/settings.h>
+#include <zephyr/shell/shell.h>
 
 LOG_MODULE_REGISTER(MODULE);
 
 void main(void)
 {
 	LOG_INF("Starting");
+
+
+	// Initialize system services, storage, settings etc.
+	settings_subsys_init();
+	settings_load();
 
 	// Start CAF and signal that main have completed the system setup
 	if (app_event_manager_init()) {
@@ -26,3 +33,14 @@ void main(void)
 
 	LOG_INF("Done");
 }
+
+// settings shell is missing a reload command to load new settings
+// after written
+static int cmd_reload(const struct shell *shell, size_t argc,
+                         char **argv)
+{
+	LOG_INF("Reloading settings");
+	settings_load();
+}
+
+SHELL_CMD_REGISTER(reload, NULL, "Reload settings", cmd_reload);
